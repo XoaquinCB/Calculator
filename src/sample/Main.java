e5fa07a64f7c4a7fb571e6c9e918daf7f97b73cb
XoaquinCastreloBrignell <xoaquin.cb@gmail.com>
2018-06-01 13:03:32 +0100

    Made changes to Main class.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,36 +1,33 @@
 package sample;
 
+import display.DisplayExample;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class Main extends Application {
-
-    private Stage stage;
-    private Scene scene;
-    private VBox layout;
-    private Label label;
-
+	
+    public static Stage stage;
+    public static Scene scene;
+    public static VBox layout;
+    
     public static void main(String[] args) {
         launch(args);
     }
-
+    
     @Override
     public void start(Stage primaryStage) {
         // Store the primaryStage in a private variable
         stage = primaryStage;
-
-        // Components
-        label = new Label("Calculator");
-
+        
         // Layout
-        layout = new VBox(15);
-        layout.getChildren().add(label);
-        layout.setAlignment(Pos.CENTER);
-
+        layout = new VBox(5);
+        layout.setAlignment(Pos.TOP_CENTER);
+        
+        DisplayExample.init();
+               
         // Scene & Stage
         scene = new Scene(layout, 300, 300);
         stage = new Stage();
